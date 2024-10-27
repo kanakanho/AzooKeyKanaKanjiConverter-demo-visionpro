0ab75a191bc3208d83143f824f45f45f64230601
kanakanho <kahoshibakichi@gmail.com>
2024-10-27 19:53:07 +0900

    feat: 入力を選択できる機能

--- AzooKeyKanaKanjiConverter-demo-2/ContentView.swift
+++ AzooKeyKanaKanjiConverter-demo-2/ContentView.swift
@@ -12,9 +12,11 @@ import KanaKanjiConverterModuleWithDefaultDictionary
 
 struct ContentView: View {
     @State var inputStr = ""
-    @State var conversionBefore = "変換前の文字列が表示されます"
-    @State var conversionResults = ["変換後の文字列が表示されます"]
+    @State var conversionResults = [""]
     @State var conversionResultsIndex = 0
+    
+    @State var complateStr = ""
+    @State var complateStrs = [""]
     
     var body: some View {
         VStack {
@@ -23,50 +25,61 @@ struct ContentView: View {
 
             Text("Hello, world!")
             
+            
+            // 変換結果を出力
+            Text(complateStr)
+            
+            
             TextField("変換する文字列を入力",text: $inputStr)
-            
-            Button(action: {
-                // 変換器を初期化する
-                let converter = KanaKanjiConverter()
-                // 入力を初期化する
-                var c = ComposingText()
-                // 変換したい文章を追加する
-                c.insertAtCursorPosition(inputStr, inputStyle: .direct)
-                // 変換前の文字列を格納する
-                conversionBefore = inputStr
-                // 変換を行う
-                let results = converter.requestCandidates(c, options: .withDefaultDictionary(requireJapanesePrediction: true, requireEnglishPrediction: false, keyboardLanguage: .ja_JP, learningType: .nothing, memoryDirectoryURL: .cachesDirectory, sharedContainerURL: .cachesDirectory, metadata: .init(appVersionString: "0.0.0")))
+                .onChange(of: inputStr) {
+                    // 変換器を初期化する
+                    let converter = KanaKanjiConverter()
+                    // 入力を初期化する
+                    var c = ComposingText()
+                    // 変換したい文章を追加する
+                    c.insertAtCursorPosition(inputStr, inputStyle: .direct)
+                    // 変換を行う
+                    let results = converter.requestCandidates(c, options: .withDefaultDictionary(requireJapanesePrediction: true, requireEnglishPrediction: false, keyboardLanguage: .ja_JP, learningType: .nothing, memoryDirectoryURL: .cachesDirectory, sharedContainerURL: .cachesDirectory, metadata: .init(appVersionString: "0.0.0")))
 
-                // 結果の一番目を表示
-                print(results.mainResults.first!.text)
-                // 変換後の文字列を格納する
-                conversionResults = results.mainResults.map(\.text)
-            }) {
-                Text("Button")
-            }
-            
-            HStack {
-                VStack{
-                    Text("変換前")
-                    Text(conversionBefore)
+                    // 結果の一番目を表示
+                    print(results.mainResults.first?.text ?? "No results")
+                    // 変換後の文字列を格納する
+                    conversionResults = results.mainResults.map(\.text)
                 }
-                .padding(.trailing, 50)
-                VStack{
-                    Text("変換後")
-                    Text(conversionResults[conversionResultsIndex])
-                    HStack{
-                        Button(action: {
-                            conversionResultsIndex = max(conversionResultsIndex - 1, 0)
-                        }) {
-                            Text("前")
-                        }
-                        Button(action: {
-                            conversionResultsIndex = min(conversionResultsIndex + 1, conversionResults.count - 1)
-                        }) {
-                            Text("次")
+            
+            HStack{
+                // 10個まで表示
+                Picker("Select", selection: $conversionResultsIndex) {
+                    let segmentedLengh = conversionResultsIndex < 10 ? 10 : 9
+                    
+                    ForEach(0..<min(conversionResults.count, segmentedLengh), id: \.self) { index in
+                        Text(conversionResults[index]).tag(index)
+                    }
+                    if (conversionResultsIndex > 10) {
+                        Text(conversionResults[conversionResultsIndex]).tag(conversionResultsIndex)
+                    }
+                }
+                
+                .pickerStyle(.segmented)
+                if conversionResults.count > 10 {
+                    Picker("More", selection: $conversionResultsIndex) {
+                        ForEach(0..<conversionResults.count, id: \.self) { index in
+                            Text(conversionResults[index])
                         }
                     }
                 }
+            }
+            
+            Button(action:{
+                if !conversionResults.isEmpty {
+                    complateStr = conversionResults[conversionResultsIndex]
+                    // 入力関係の初期化
+                    inputStr = ""
+                    conversionResults = [""]
+                    conversionResultsIndex = 0
+                }
+            }){
+                Text("確定")
             }
 
             ToggleImmersiveSpaceButton()
